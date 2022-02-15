f04deac7cd7f51304a6699e74b5c5bb03ae00307
Raja Nagendra Kumar <nagendra.raja@tejasoft.com>
2022-02-15 09:11:51 +0530

    included .idea files

---
+++ grpc/app/src/main/java/gae/piaz/grpc/client/WebRequestController.java
@@ -0,0 +1,19 @@
+package gae.piaz.grpc.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRequestController
+{
+    @Autowired
+    private WebRequestHandler webClientService;
+
+    @RequestMapping("/")
+    public String printMessage(@RequestParam(defaultValue = "Tano") String name)
+    {
+	return webClientService.sendMessage(name);
+    }
+}
